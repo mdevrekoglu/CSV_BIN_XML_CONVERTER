1f729f367a3ec522e43da4ef7c3a30aac24e32ec
Mehmet Devrekoglu <mehmetdevrekoglu01@gmail.com>
2023-04-26 13:47:18 +0300

    final v1

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
 run: program.c
-	gcc -o program program.c -I/usr/include/libxml2 -lxml2
-	./program
+	gcc -o program program.c -I/usr/include/libxml2 -lxml2 -lm
+	./program records.csv binary.dat 1
+	./program binary.dat records.xml 2
+	./program records.xml records.xsd 3
+	
--- program.c
+++ program.c
@@ -1,93 +1,339 @@
-#include <stdio.h>
-#include <libxml/xmlschemastypes.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdbool.h> // boolean type
+#include <string.h> // string operations
+#include <stdio.h> // standart buffered input/output
+#include <string.h> // string operations
+#include <stdlib.h> // standart library definitions
+#include <math.h> // math operations
+#include <libxml/xmlschemastypes.h> //xml schema types
 
-int validate(char *XMLFileName, char *XSDFileName);
-int convertCSVtoBinary(char *CSVFileName, char *BinaryFileName);
-int convertSTRtoBIN();
+void convertCSVtoBIN(char *CSVFileName, char *BinaryFileName);
+void converBINtoXML(char *BinaryFileName, char *XMLFileName);
+void validate(char *XMLFileName, char *XSDFileName);
 
-// Our project is reading an csv file and converting it to binary file then reading that binary file and
-// Converting it to xml file and then validating it with xsd file. The language we are using is C.
-int main()
+typedef struct Person
 {
-    char *CSVFileName = "records.csv";
-    char *BinaryFİleName = "binary.bin"; //!!!!!!!!!!
-    char *XMLFileName = "records.xml"; // write your xml file here
-    char *XSDFileName = "records.xsd"; // write your xsd file here
+    char name[21];
+    char surname[31];
+    char gender;
+    char occupancy[31];
+    char level_of_education[4];
+    char email[31];
+    char bank_account_number[13];
+    char IBAN[28];
+    char account_type[14];
+    char currency_unit[4];
+    unsigned int total_balance_available;
+    char available_for_loan[5];
+}Person;
 
-    // send copy of the string to the function
-    char str[strlen(CSVFileName)];
-    strcpy(str, CSVFileName);
-    int a = convertSTRtoBIN(str);
+// Main function
+int main(int argc, char *argv[])
+{
+    if(argc != 4){ // Checking whether given arguments are correct
+        printf("Wrong number of arguments\n");
+        printf("Usage: ./program <input_file> <output_file> <type>\n");
+        printf("Type: 1 - CSV to BIN\n");
+        printf("Type: 2 - BIN to XML\n");
+        printf("Type: 3 - XML XSD verification\n");
+        exit(-1);
+    }
 
+    int type = atoi(argv[3]); // Converting last argument to integer
+    if(type == 1){ // If type is 1 then convert CSV to BIN
+        convertCSVtoBIN(argv[1], argv[2]);
+    }
+    else if(type == 2){ // If type is 2 then convert BIN to XML
+        converBINtoXML(argv[1], argv[2]);
+    }
+    else if(type == 3){ // If type is 3 then validate XML with XSD
+        validate(argv[1], argv[2]);
+    }
+    else{ // If type is not 1, 2 or 3 then print error message
+        printf("Wrong type\n");
+        printf("Type: 1 - CSV to BIN\n");
+        printf("Type: 2 - BIN to XML\n");
+        printf("Type: 3 - XML XSD verification\n");
+        exit(-1);
+    }
 
-    // convert str to char array
-
-
-
-
-    //int BinaryFileState = convertCSVtoBinary(CSVFileName, BinaryFİleName);
-    //printf("Binary State: %d\n", BinaryFileState);
-
-    //int ValidateState = validate(XMLFileName, XSDFileName);
-    //printf("Validate State: %d\n", ValidateState);
-    
-    return 0;  
+    return 0; 
 }
 
-int convertCSVtoBinary(char *CSVFileName, char *BinaryFileName){
+// Convert CSV to BIN
+void convertCSVtoBIN(char *CSVFileName, char *BINFileName){
 
-    FILE* fpCSV = fopen(CSVFileName, "r");
-    FILE* fpBIN = fopen(BinaryFileName, "w");
+    FILE *CSVFile = fopen(CSVFileName, "r");
+    FILE *BINFile = fopen(BINFileName, "wb");
 
-    if(!fpCSV){
-        printf("An error accoured during opening %s\n", CSVFileName);
-        return -1;
+    if(!CSVFile){
+        printf("Failed to open CSV file.\n");
+        exit(1);
     }
-    if(!fpBIN){
-        printf("An error accoured during opening %s\n", BinaryFileName);
-        return -1;
+    if (!BINFile) {
+        printf("Failed to create binary file.\n");
+        exit(1);
     }
 
-    // Assuming every line has maximum 1000 characters
-    char row[1000];
-    char bin[1000];
+    char line[sizeof(Person)];
+    char temp[sizeof(Person)]; // temporary for modified line
+
+    // Clear the line and temp strings
+    memset(&temp, 0, sizeof(Person));
+    memset(&line, 0, sizeof(Person));
+     
+    Person person;
+    
+    fgets(line, sizeof(Person), CSVFile);
+    while (fgets(line, sizeof(Person), CSVFile)) {
+        int len = strlen(line);
 
 
-    while(feof(fpCSV) != true){
-        fgets(row, 1000, fpCSV);
-        printf("Row: %s",row);
-
-        char * token = strtok(row, ",");
-
-        while(token != NULL){
-            printf("%s ", token);
-            token = strtok(NULL, ",");
+        for (int i = 0; i < len-1; i++) {
+            if (line[i] == ',' && line[i+1] == ',') {
+                // insert "NULL" between the two commas
+                
+                memcpy(temp, line, i+1); // copy the part of the line up to the first comma
+                strcpy(temp+i+1, " "); // insert "NULL" after the first comma
+                strcpy(temp+i+2, line+i+1); // copy the part of the line after the second comma
+                strcpy(line, temp); // copy the modified line back to the original buffer
+                len += 1; // update the length of the line
+            }
         }
-        printf("\n");
+
+
+        // Read values from line and store them in person
+        // Also get the result of sscanf to check whether there is an error or not
+        int result = sscanf(line, "%[^,],%[^,],%c,%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%[^,],%u,%[^,\n]",
+            person.name, person.surname, &person.gender, person.occupancy, person.level_of_education,
+            person.email, person.bank_account_number, person.IBAN, person.account_type,
+            person.currency_unit, &person.total_balance_available, person.available_for_loan);
+
+        // If result is not 12 and row is not empty then print error message
+        if(result != 12 && line[0] != '\n' && line[0] != '\0'){
+            printf("\nAn error accoured during reading a row\n");
+            printf("Row: %s\n", line);
+            printf("Result: %d\n\n", result);
+            continue;
+        }
+    
+        // If any of the values of person is equal to ' ' then change it to NULL, ""
+        if(strcmp(person.name, " ") == 0){
+            strcpy(person.name, "");
+        }
+        if(strcmp(person.surname, " ") == 0){
+            strcpy(person.surname, "");
+        }
+        if(strcmp(person.occupancy, " ") == 0){
+            strcpy(person.occupancy, "");
+        }
+        if(strcmp(person.level_of_education, " ") == 0){
+            strcpy(person.level_of_education, "");
+        }
+        if(strcmp(person.email, " ") == 0){
+            strcpy(person.email, "");
+        }
+        if(strcmp(person.bank_account_number, " ") == 0){
+            strcpy(person.bank_account_number, "");
+        }
+        if(strcmp(person.IBAN, " ") == 0){
+            strcpy(person.IBAN, "");
+        }
+        if(strcmp(person.account_type, " ") == 0){
+            strcpy(person.account_type, "");
+        }
+        if(strcmp(person.currency_unit, " ") == 0){
+            strcpy(person.currency_unit, "");
+        }
+        if(strcmp(person.available_for_loan, " ") == 0){
+            strcpy(person.available_for_loan, "");
+        }
+
+        fwrite(&person, sizeof(Person), 1, BINFile);
+
+        // Clear the line and temp strings
+        memset(&temp, 0, sizeof(Person));
+        memset(&line, 0, sizeof(Person));
     }
 
+    fclose(CSVFile);
+    fclose(BINFile);
+    
+    // Print the result
+    printf("CSV to BIN conversion is completed\n");
+}
 
-    fclose(fpCSV);
+// Convert BIN to XML
+void converBINtoXML(char *BINFileName, char *XMLFileName){
+
+    // Open the given file in binart reading mode
+    FILE *fpBIN = fopen(BINFileName, "rb");
+
+    // Check if the file is opened
+    if(!fpBIN){
+        printf("An error accoured during opening %s\n", BINFileName);
+        exit(-1);
+    }
+
+    // To calculate number of person go end of the file and divide by size of Person
+    // It is not necessary but it is good to know
+    fseek(fpBIN, 0, SEEK_END);
+    int numberOfPerson = (ftell(fpBIN)/sizeof(Person));
+
+    // There is an errow which couses to remove one person from the file because of the last line
+    // To avoid it number of person can be decreased by one
+    // But I assume there is not any error in the file. What I mean is the last line is not empty
+    // So I don't decrease the number of person
+    // numberOfPerson--;
+    
+    // For debugging
+    // printf("Number of person: %d\n", numberOfPerson);
+
+    // Go back to the beginning of the file
+    fseek(fpBIN, 0, SEEK_SET);
+
+    // Create XML document and root node
+    xmlDocPtr doc = NULL;
+    xmlNodePtr rootNode = NULL;
+    
+    // Create a new XML document and a new root node
+    doc = xmlNewDoc(BAD_CAST "1.0");
+
+    // In this part root node is created
+    rootNode = xmlNewNode(NULL, BAD_CAST "records");
+    // Set the root node
+    xmlDocSetRootElement(doc, rootNode);
+
+    // !! Attention !! due to document the root node's name should be the same as the file name
+    // But on xsd file it can't be the same as the file name
+    // Therefore xsd file can't validate the xml file
+    // To solve this problem I changed the root node's name to records
+    // But there is a code that makes the main root's name as file name
+    // It is commented out
+
+    // char *tempFileName = strtok(BINFileName, ".");
+    // rootNode = xmlNewNode(NULL, BAD_CAST XMLFileName);
+
+
+    // Read from file and write to XML
+    for(int i = 1; i <= numberOfPerson; i++){
+        // Create a temporary structure
+        Person person;
+        // Clear the structure, we don't want garbage values
+        memset(&person, 0, sizeof(Person));
+        // Read from file in binary format and store it in person
+        fread(&person, sizeof(Person), 1, fpBIN);
+
+        // Converting variables
+        char intToStr[11]; // 10 digits + '\0'
+        char charToStr[2]; // 1 digit + '\0'
+        intToStr[10] = '\0';
+        charToStr[1] = '\0';
+        
+        // Create a new row node and set its attribute
+        xmlNodePtr tempNode = xmlNewChild(rootNode, NULL, BAD_CAST "row", NULL);
+        sprintf(intToStr, "%d", i);
+        xmlNewProp(tempNode, BAD_CAST "id", BAD_CAST intToStr);
+
+        // Create a subnode for row node
+        xmlNodePtr customerInfo = xmlNewChild(tempNode, NULL, BAD_CAST "customer_info", NULL);
+
+        // Create subnodes for customerInfo subnode and set their values
+        xmlNewChild(customerInfo, NULL, BAD_CAST "name", BAD_CAST person.name);
+        xmlNewChild(customerInfo, NULL, BAD_CAST "surname", BAD_CAST person.surname);
+        charToStr[0] = person.gender;
+        xmlNewChild(customerInfo, NULL, BAD_CAST "gender", charToStr);
+        xmlNewChild(customerInfo, NULL, BAD_CAST "occupancy", BAD_CAST person.occupancy);
+        xmlNewChild(customerInfo, NULL, BAD_CAST "level_of_education", BAD_CAST person.level_of_education);
+        xmlNewChild(customerInfo, NULL, BAD_CAST "email", BAD_CAST person.email);
+
+        // Create a subnode for row node
+        xmlNodePtr bankAccountInfo = xmlNewChild(tempNode, NULL, BAD_CAST "bank_account_info", NULL);
+
+        // Create subnodes for bankAccountInfo subnode and set their values
+        xmlNewChild(bankAccountInfo, NULL, BAD_CAST "bank_account_number", BAD_CAST person.bank_account_number);
+        xmlNewChild(bankAccountInfo, NULL, BAD_CAST "IBAN", BAD_CAST person.IBAN);
+        xmlNewChild(bankAccountInfo, NULL, BAD_CAST "account_type", BAD_CAST person.account_type);
+        
+        // This node has an attribute so we need to create it separately
+        sprintf(intToStr, "%d", person.total_balance_available);
+        xmlNodePtr a = xmlNewChild(bankAccountInfo, NULL, BAD_CAST "total_balance_available", BAD_CAST intToStr);
+        // Set the first attribute
+        xmlNewProp(a, BAD_CAST "currency_unit", BAD_CAST person.currency_unit);
+        
+        // Unsigned int to binary string (4 bytes)
+        unsigned int littleEnd_Version = person.total_balance_available;  
+        // Little Endian to Big Endian (4 bytes) conversion part   
+		char binarySTR[33]; // 32 digits + '\0'
+		binarySTR[32] = '\0';
+		int m = 31, n; // Variables to traverse the string
+		while(littleEnd_Version != 0){ // Convert to binary
+			if(littleEnd_Version % 2 == 0){
+				binarySTR[m] = '0';
+			}
+			else{
+				binarySTR[m] = '1';
+			}
+
+			littleEnd_Version /= 2;
+			m--;
+		}
+		while(m >= 0){ // Fill the rest of the string with 0s to remove garbage values
+			binarySTR[m] = '0';
+			m--;
+		}
+
+        // For debugging
+        //printf("Little End Str: %s\n", binarySTR);
+
+        m = 0, n = 31; //Reset variables
+        while(m < n){ // Reverse the string to get Big Endian
+            char temp = binarySTR[m];
+            binarySTR[m] = binarySTR[n];
+            binarySTR[n] = temp;
+
+            m++;
+            n--;
+        }
+
+        // For debugging
+        //printf("BigEnd Str: %s\n", binarySTR);
+
+        // Binary string to unsigned int (4 bytes)
+        unsigned int bigEnd_Version = 0;
+        m = 0, n = 31; // Reset variables
+        while(m <= n){ // Convert to unsigned int
+            if(binarySTR[m] == '1'){
+                bigEnd_Version += (int)pow((double) 2, (double) (31 - m));
+            }
+            m++;
+        }
+
+        // For debugging
+        // printf("BigEnd Int: %u\n", bigEnd_Version);
+
+        sprintf(intToStr, "%u", bigEnd_Version);
+        xmlNewProp(a, BAD_CAST "bigEnd_Version", BAD_CAST intToStr);
+        
+        xmlNewChild(bankAccountInfo, NULL, BAD_CAST "available_for_loan", BAD_CAST person.available_for_loan);
+    }
+
+    // Save the XML file
+    xmlSaveFormatFileEnc(XMLFileName, doc, "UTF-8", 1);
+
+    // Print the XML file to the console (for debugging)
+    // xmlSaveFormatFileEnc("-", doc, "UTF-8", 1);
+
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
     fclose(fpBIN);
 
-    return 0;
+    // Print the result
+    printf("XML file created successfully.\n");
 }
 
-int convertSTRtoBIN(char str[]){
+// Validate the XML with XSD
+void validate(char *XMLFileName, char *XSDFileName){
     
-
-    for (int i = 0; i < strlen(str); i++)
-    {
-        printf("%c", str[i]);
-    }
-    
-
-    return 0;
-}
-
-int validate(char *XMLFileName, char *XSDFileName){
     xmlDocPtr doc;
     xmlSchemaPtr schema = NULL;
     xmlSchemaParserCtxtPtr ctxt;    
@@ -131,6 +377,4 @@ int validate(char *XMLFileName, char *XSDFileName){
     xmlSchemaCleanupTypes(); //cleanup the default xml schemas types library
     xmlCleanupParser(); //cleans memory allocated by the library itself 
     xmlMemoryDump(); //memory dump
-    return(0);
 }
-
